0a37952f0706fbb031b7af4c2a0728c792545e2b
George <bongostar41@gmail.com>
2016-05-14 16:49:48 -0400

    added angular

--- js/index.js
+++ js/index.js
@@ -1,16 +1,84 @@
 
 
+angular.module('SampleApp', [])
+
+.controller('SampleController', function($filter, CONVERSATIONS) {
+	var self = this;
+
+	self.conversations = CONVERSATIONS;
+
+	(function init() {
+		self.currentlyViewing = self.conversations[0];
+		self.currentlyViewing.isActive = true;
+	})();
+
+	self.viewConversation = function(conversation) {
+		self.currentlyViewing.isActive = false;
+		self.currentlyViewing.isNew = false;
+		conversation.isActive = true;
+		self.currentlyViewing = conversation;
+	};
+
+	self.addMessage = function(conversation, message) {
+		if (message) {
+			conversation.messageHistory.push({
+				isRecieved: false,
+				message: message,
+				timestamp: $filter('date')(Date.now(), 'h:MM a'),
+				datestamp: $filter('date')(Date.now(), 'M/d/yyyy'),
+			});
+			self.userMessage = null;
+		}
+	};
+})
+
+.directive('contenteditable', ['$sce', function($sce) {
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: '?ngModel', // get a hold of NgModelController
+    link: function(scope, element, attrs, ngModel) {
+      if (!ngModel) return; // do nothing if no ng-model
+
+      // Specify how UI should be updated
+      ngModel.$render = function() {
+        element.text(ngModel.$viewValue || '');
+        setNewHeight();
+      };
+
+      // Listen for change events to enable binding
+      element.on('blur keydown change', function() {
+        scope.$eval(read);
+
+      });
+      read(); // initialize
+
+      // Write data to the model
+      function read() {
+        var html = element.text();
+        // When we clear the content editable the browser leaves a <br> behind
+        // If strip-br attribute is provided then we strip this out
+        if ( attrs.stripBr && html == '<br>' ) {
+          html = '';
+        }
+        ngModel.$setViewValue(html);
+      }
+
+      var headerHeight = 38;
+      var textArea = element[0];
+      var messageCenter = document.getElementById('message-center');
+
+      function setNewHeight() {
+      	var messageSize = textArea.parentElement.getBoundingClientRect().height;
+      	messageCenter.style.height = 'calc( 100% - ' + (messageSize + headerHeight)+ 'px)';
+      }
+      
+      textarea.addEventListener('input', setNewHeight);
+      window.addEventListener('resize', setNewHeight);
+    }
+  };
+}]);
+
 
 document.addEventListener("DOMContentLoaded", function() {
-	var headerHeight = 38;
-	var textArea = document.getElementById('textarea');
-	var messageCenter = document.getElementById('message-center');
 
-	textarea.addEventListener('input', setNewHeight);
-	window.addEventListener('resize', setNewHeight);
-
-	function setNewHeight() {
-		var messageSize = textArea.parentElement.getBoundingClientRect().height;
-		messageCenter.style.height = 'calc( 100% - ' + (messageSize + headerHeight)+ 'px)';
-	}
 });
---
+++ js/services.js
@@ -0,0 +1,155 @@
+angular.module('SampleApp')
+
+.value('CONVERSATIONS', [{
+	isNew: true,
+	sender: {
+		name: 'Starbucks - Bill M'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: [{
+		isRecieved: false,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, se',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}, {
+		isRecieved: true,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit essillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}, {
+		isRecieved: false,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, se',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}, {
+		isRecieved: true,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit essillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}, {
+		isRecieved: false,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, se',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}, {
+		isRecieved: true,
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit essillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}]
+}, {
+	isNew: false,
+	sender: {
+		name: 'Coca Cola - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: [{
+		isRecieved: true,
+		message: 'rem ipsum dolor sit amet, consectetur adipisicing elit, se',
+		timestamp: '2:54 PM',
+		datestamp: '3/14/2016'
+	}]
+}, {
+	isNew: false,
+	sender: {
+		name: 'Adobe - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'System Message1'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'Spotify - Mike M'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: false,
+	sender: {
+		name: 'Coca Cola - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: false,
+	sender: {
+		name: 'Adobe - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'System Message1'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'Spotify - Mike M'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: false,
+	sender: {
+		name: 'Coca Cola - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: false,
+	sender: {
+		name: 'Adobe - Mike K'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'System Message1'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}, {
+	isNew: true,
+	sender: {
+		name: 'Spotify - Mike M'
+	},
+	messageHeading: {
+		message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven'
+	},
+	messageHistory: []
+}]);
